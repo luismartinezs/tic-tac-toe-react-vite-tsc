d2343992210efadca3880e13fa4bc048ad325a6b
Luis Martinez <luis@passionatepeople.io>
2022-05-12 08:23:39 +0200

    feat: start and finish toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import GameFinished from '@/components/GameFinished'
-import GameStart from '@/components/GameStart'
-
-const Game = GameStart
-
+import GameFinished from "@/components/GameFinished";
+import GameStart from "@/components/GameStart";
+import { useState } from "react";
 
 function App(): JSX.Element {
+  const [showFinished, setShowFinished] = useState(true);
 
   return (
     <div>
-      <Game />
+      <button onClick={() => setShowFinished(!showFinished)} className="mb-8 bg-primary-500 text-white px-4 py-2 rounded">
+        Show {showFinished ? "start" : "finish"}
+      </button>
+      {showFinished ? <GameFinished /> : <GameStart />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
